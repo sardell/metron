aca36c8d3b55f3e3c2a79230e2774f372c095dd5
Shane Ardell <sardell@hortonworks.com>
2019-03-25 16:49:13 +0100

    remove old shared workspace

--- metron-interface/metron-shared/projects/metron-shared-lib/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of metron-shared-lib
- */
-
-export * from './lib/metron-shared-lib.service';
-export * from './lib/metron-shared-lib.component';
-export * from './lib/metron-shared-lib.module';
